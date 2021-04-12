8016ea14c8eebd9249dfa3a5f255456b5baf1ac9
Naman Jain <n27jain@uwaterloo.ca>
2021-04-12 00:41:38 -0400

    NJ changes

--- CarND-Path-Planning-Project-master/build/cmake_install.cmake
+++ CarND-Path-Planning-Project-master/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/namanjain/Downloads/CarND-Path-Planning-Project-master
+# Install script for directory: /Users/namanjain/Downloads/AutonomousDrivingLaneChange/CarND-Path-Planning-Project-master
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/namanjain/Downloads/CarND-Path-Planning-Project-master/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/namanjain/Downloads/AutonomousDrivingLaneChange/CarND-Path-Planning-Project-master/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
